c857d939b19282d458bcc14256fe607b0df6371e
sarahlshowers <sarahlshowers@gmail.com>
2015-08-23 15:56:00 -0700

    top rated artisits by a singluar genre

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -7,6 +7,7 @@ export default DS.RESTAdapter.extend({
     var url = this.buildURL(type.typeKey);
     var singularURL = url.substring(0, url.length-1);
     queryParams.api_key = 'RIDOQTZNDLWH4BHYH';
-    return this.ajax([singularURL, 'search'].join('/'), 'GET', {data: queryParams})
+    console.log(queryParams);
+    return this.ajax([singularURL, 'artists'].join('/'), 'GET', {data: queryParams})
   },
 });
--- app/routes/sonorous.js
+++ app/routes/sonorous.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.find('song', {
-      artist: 'Radiohead'
+    return this.store.find('genre', {
+      name: 'jazz',
+      bucket: 'hotttnesss',
+      results: '10'
     })
   }
 });
