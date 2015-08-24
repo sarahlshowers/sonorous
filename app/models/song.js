1bf88f7a4dc8498255547a099c56bf23e9dddc20
sarahlshowers <sarahlshowers@gmail.com>
2015-08-23 17:02:26 -0700

    Render query results
    - forcing namespace to hit genre (api is weird)
    - no need to unpluralize artists, removing
    - update what is being returned from function to singluar, forced url
    
    - create artist model
    - delete genre model
    
    - cleaning console.log statements
    
    - adding genres to template

--- app/models/song.js
+++ app/models/song.js
@@ -1,7 +1,7 @@
-// import DS from 'ember-data';
+import DS from 'ember-data';
 
-// export default DS.Model.extend({
-//   artist_id: DS.attr(),
-//   artist_name: DS.attr(),
-//   title: DS.attr()
-// });
+export default DS.Model.extend({
+  artist_id: DS.attr(),
+  artist_name: DS.attr(),
+  title: DS.attr()
+});
--- app/routes/sonorous.js
+++ app/routes/sonorous.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.find('genre', {
+    return this.store.find('artist', {
       name: 'jazz',
       bucket: 'hotttnesss',
       results: '10'
