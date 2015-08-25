89c0d045d4addd53573e883116ed7a78cea44a8a
Sarah Showers <sarah_showers@apple.com>
2015-08-24 22:25:58 -0700

    changed to select, get selected value

--- app/routes/country.js
+++ app/routes/country.js
@@ -4,8 +4,7 @@ export default Ember.Route.extend({
     model: function () {
     return this.store.find('artist', {
       name: 'country',
-      bucket: 'hotttnesss',
-      results: '10'
+      bucket: 'artist_location',
     })
   }
 });
