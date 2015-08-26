b6d16b4a818b5b7310b85a400b14c8673475c436
Sarah Showers <sarah_showers@apple.com>
2015-08-26 08:17:31 -0700

    Return filtered results

--- app/controllers/country.js
+++ app/controllers/country.js
@@ -2,5 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   application: Ember.inject.controller(),
-  stateValue: Ember.computed.alias('application.stateValue')
+  stateValue: Ember.computed.alias('application.stateValue'),
+  filteredArtists: function() {
+    var artistState = this.get('stateValue');
+    var artists = this.get('model');
+
+    if (artistState) {
+      console.log('hello', artists);
+      return artists.filterBy('artist_location.region', artistState);
+    } else {
+      console.log('world', artists);
+      return artists;
+    }
+  }.property('stateValue')
 });
