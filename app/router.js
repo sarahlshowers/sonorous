7226adb0dea163a0bae0136b12a34d65f26c7096
Sarah Showers <sarah_showers@apple.com>
2015-08-25 18:45:04 -0700

    update root route to sonorous route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('sonorous');
+  this.resource('sonorous', {path: '/'});
   this.route('jazz');
   this.route('rock');
   this.route('country');
